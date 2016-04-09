d8b703b52474ac09895d178d297e816a51a32558
pocari <caffelattenonsugar@gmail.com>
2016-04-09 16:20:51 +0900

    フォルダ名変更

--- docker/bin/common.sh
+++ docker/bin/common.sh
@@ -1,5 +1,5 @@
 SCRIPT_DIR=$(cd $(dirname $0) && pwd)
-export APP_ROOT_DIR=$(dirname $(dirname $SCRIPT_DIR))/app/myapp
+export APP_ROOT_DIR=$(dirname $(dirname $SCRIPT_DIR))/app/rails
 export DB_DOCKERFILE_DIR=${SCRIPT_DIR}/../containers/db
 export WEBAPP_DOCKERFILE_DIR=${SCRIPT_DIR}/../containers/webapp
 export DOCKER_COMMAND=docker
