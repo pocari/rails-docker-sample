6d5dba36eefdfa7c2fcee426bd1a698f2076ca24
pocari <caffelattenonsugar@gmail.com>
2016-04-09 23:24:54 +0900

    不要なコンテナ削除

--- docker/bin/common.sh
+++ docker/bin/common.sh
@@ -2,7 +2,6 @@ SCRIPT_DIR=$(cd $(dirname $0) && pwd)
 export APP_ROOT_DIR=$(dirname $(dirname $SCRIPT_DIR))/app/rails
 export DB_DOCKERFILE_DIR=${SCRIPT_DIR}/../containers/db
 export WEBAPP_DOCKERFILE_DIR=${SCRIPT_DIR}/../containers/webapp
-export DATA_CONTAINER_DOCKERFILE_DIR=${SCRIPT_DIR}/../containers/data_container
 export DOCKER_COMMAND=docker
 
 echo APP_ROOT_DIR = ${APP_ROOT_DIR}
